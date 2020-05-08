73dc399c3502ad9db08ad70394f1ca8565029de7
Anatoliy Gatt <anatoliy@gatt.io>
2020-05-08 16:30:44 +0100

    Enhance deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker build -t anatoliygatt/cicd-docker-nginx:$TRAVIS_TAG ./nginx
-docker build -t anatoliygatt/cicd-docker-api:$TRAVIS_TAG ./api
+docker build -t anatoliygatt/cicd-docker-nginx:$TRAVIS_TAG -t anatoliygatt/cicd-docker-nginx ./nginx
+docker build -t anatoliygatt/cicd-docker-api:$TRAVIS_TAG -t anatoliygatt/cicd-docker-api ./api
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
-docker push anatoliygatt/cicd-docker-nginx:$TRAVIS_TAG
-docker push anatoliygatt/cicd-docker-api:$TRAVIS_TAG
+docker push anatoliygatt/cicd-docker-nginx
+docker push anatoliygatt/cicd-docker-api
