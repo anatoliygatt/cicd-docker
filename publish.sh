3f623286886efff7771338731ffe4366fe5af5fa
Anatoliy Gatt <anatoliy@gatt.io>
2020-05-09 13:18:47 +0100

    Replace deploy with after_success

---
+++ publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ev
+if [ "$TRAVIS_BRANCH" = "master" && "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_TAG}" != "" ]; then
+  docker build -t anatoliygatt/cicd-docker-nginx:$TRAVIS_TAG -t anatoliygatt/cicd-docker-nginx ./nginx
+  docker build -t anatoliygatt/cicd-docker-api:$TRAVIS_TAG -t anatoliygatt/cicd-docker-api ./api
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+  docker push anatoliygatt/cicd-docker-nginx
+  docker push anatoliygatt/cicd-docker-api
+fi
