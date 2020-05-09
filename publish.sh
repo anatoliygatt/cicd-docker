b4dc812cb60b3fcc6a77c224af606c8a7a111fff
Anatoliy Gatt <anatoliy@gatt.io>
2020-05-09 13:48:11 +0100

    fix

--- publish.sh
+++ publish.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo $TRAVIS_BRANCH
-echo $TRAVIS_PULL_REQUEST
-echo $TRAVIS_TAG
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ ! -z "${TRAVIS_TAG}" ]; then
+if [ ! -z "${TRAVIS_TAG}" ]; then
   docker build -t anatoliygatt/cicd-docker-nginx:$TRAVIS_TAG -t anatoliygatt/cicd-docker-nginx ./nginx
   docker build -t anatoliygatt/cicd-docker-api:$TRAVIS_TAG -t anatoliygatt/cicd-docker-api ./api
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
