f921656329c62d01d9d4cfbc93ad8fb72a33c307
Anatoliy Gatt <anatoliy@gatt.io>
2020-05-11 19:46:34 +0100

    fix

--- api/src/index.js
+++ api/src/index.js
@@ -14,6 +14,10 @@ const pgClient = new Pool({
   port: process.env.PGPORT,
 });
 
+pgClient
+  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+  .catch((err) => console.log(err));
+
 app.set("port", process.env.PORT || 3000);
 
 const server = new ApolloServer({
